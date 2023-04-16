a0971a7c729ce8813a279f8dc2c2e4cf1dcb37bb
воробйов александр <avorobeov21@gmail.com>
2023-04-16 19:39:35 +0300

    Добавьте файлы проекта.

---
+++ Homework_number_16/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_number_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int maximumRandomNumber = 27;
+            int minimalRandomNumber = 1; 
+            int randomNumber = random.Next(minimalRandomNumber, maximumRandomNumber);
+            int startIndex = 100;
+            int endIndex = 1000;
+            int numberOfMultiples = 0;
+            int remainder;
+
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                remainder = i;
+                
+                while (remainder >0)
+                {
+                    remainder -= randomNumber;
+
+                    if (remainder == 0)
+                    {
+                        numberOfMultiples++;
+                    }
+                }
+            }
+        }
+    }
+}
